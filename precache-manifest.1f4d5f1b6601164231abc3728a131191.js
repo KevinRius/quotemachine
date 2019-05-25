22a40d0c36835e8219c6dc6648322d0ebc78bd8c
KevinRius <k.riou29@gmail.com>
2019-05-26 01:59:58 +0900

    Updates

---
+++ precache-manifest.1f4d5f1b6601164231abc3728a131191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34923a666d2b9b88b8553d6452c7c8f",
+    "url": "/quotemachine/index.html"
+  },
+  {
+    "revision": "0d08d86aa7da10bbfbd3",
+    "url": "/quotemachine/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3432265c711bc75c368e",
+    "url": "/quotemachine/static/js/2.4f4337f1.chunk.js"
+  },
+  {
+    "revision": "0d08d86aa7da10bbfbd3",
+    "url": "/quotemachine/static/js/main.cb2262a1.chunk.js"
+  },
+  {
+    "revision": "667595373df25bfb8ae4",
+    "url": "/quotemachine/static/js/runtime~main.4d0fe069.js"
+  }
+]);
